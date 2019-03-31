e4bcb1bd65d6a56504fe7767ad14bc7dd8a83d20
danielrussocki <46513699+danielrussocki@users.noreply.github.com>
2019-03-31 17:02:00 -0500

    Vista Footer parte 1 terminada
    -Se agregaron los fonts necesarios para el template.
    -Se agregaron imágenes necesarias para el footer.
    -index.html modificado.
    -El scroll down ya funciona de forma correcta.
    -Archivos modificados: style.css, fonts.css, main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ $('header nav ul li').click(function(){
     $('header nav ul li').removeClass('active');
     $(this).addClass('active');
 });
+$(document).ready(function(){
+    scrolling();
+});
+function scrolling(){
+    $('.scroll-down').click(function(){
+        $('html,body').animate({
+            scrollTop: $('.footer-1').offset().top
+        }, 1500);
+    });
+}
