4d3f109533e15ae2f25bf9483b1267faee4dfe7d
danielrussocki <46513699+danielrussocki@users.noreply.github.com>
2019-04-04 15:57:18 -0500

    Unión de las secciones
    -Se juntaron las secciones de cada miembro del equipo.
    -Se comenzó a hacer la base del dashboard.
    -Se añadió contenido digital(Imágenes).

---
+++ backend/usuarios.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+    <link rel="stylesheet" href="css/dashbar.css">
+</head>
+<body>
+    <div class="grid-container">
+        <header class="header">
+            <h1 class="header__title">Dashboard</h1>
+        </header>
+        <aside class="sidenav">
+            <ul class="sidenav__list">
+                <li class="sidenav__list-item"><a href="#">usuarios</a></li>
+                <li class="sidenav__list-item"><a href="#">navigation bar</a></li>
+                <li class="sidenav__list-item"><a href="#">header</a></li>
+                <li class="sidenav__list-item"><a href="#">about us</a></li>
+                <li class="sidenav__list-item"><a href="#">posts</a></li>
+                <li class="sidenav__list-item"><a href="#">address</a></li>
+                <li class="sidenav__list-item"><a href="#">contact us</a></li>
+                <li class="sidenav__list-item"><a href="#">etc</a></li>
+            </ul>
+        </aside>
+        <main class="main">
+            <div class="main-overview">
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+                <div class="overviewcard">
+                    <h3 class="overviewcard__icon">Overview</h3>
+                    <div class="overviewcard__info">Text Card</div>
+                </div>
+            </div>
+        </main>
+        <footer class="footer">
+            <div class="footer__copyright">&copy; 2019 DRS</div>
+            <div class="footer__signature">Made it by the pure team 4</div>
+        </footer>
+    </div>
+    <!-- <aside class="sidebar-dashboard">
+        <div class="logo-dash">
+            <img src="img/team4-logo.png" alt="logo">
+        </div>
+        <ul>
+            <li><i class=""></i><a href="#">usuarios</a></li>
+            <li><i class=""></i><a href="#">navigation bar</a></li>
+            <li><i class=""></i><a href="#">header</a></li>
+            <li><i class=""></i><a href="#">about us</a></li>
+            <li><i class=""></i><a href="#">posts</a></li>
+            <li><i class=""></i><a href="#">address</a></li>
+            <li><i class=""></i><a href="#">contact us</a></li>
+            <li><i class=""></i><a href="#">etc</a></li>
+        </ul>
+    </aside>
+    <main class="main-dashboard-container">
+        <h1 class="dash-title">usuarios</h1>
+        <div class="mostrar-tabla">
+            <div class="btn-union">
+                <button class="boton btn-nuevo">nuevo</button>
+                <button class="boton btn-cancelar">cancelar</button>
+            </div>
+            <table class="dash-table">
+                <tr>
+                    <th>Correo</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                </tr>
+                <tr>
+                    
+                </tr>
+            </table>
+        </div>
+        <div class="insertar-datos">
+            <form>
+                <input type="email">
+                <input type="password">
+                <input type="text">
+                <input type="text">
+            </form>
+        </div>
+    </main> -->
+</body>
+</html>
