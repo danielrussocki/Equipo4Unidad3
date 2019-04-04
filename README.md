2fd4e56b6b52f785cb98891c5b0fb6017cb8b7e0
EOjeda95 <ed.ojeda95@gmail.com>
2019-04-04 13:28:42 -0600

    Revert "FIX"
    This reverts commit 1c64e097508444de00e2dc5de6642cee148fd724.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Equipo4Unidad3
-DesarrolloWeb
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+     <link rel="stylesheet" href="estilos.css">
+<main class="main">
+
+    <div class="section-what-we-do">
+     <div class="container container-centered">
+       <h2 id="what-we-do" class="section-title">What we do</h2>
+       <hr class="line">
+       <p class="section-description">Lorem ipsum dolor sit amet. Proin gravida nibh vel velit auctor aliquet.</p>
+       <div class="lorem">
+         <div class="lorem-logo lorem-camera"></div>
+         <h3>Lorem Ipsum</h3>
+         <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+         <a class="learn_more" href="#">Learn more</a>
+       </div>
+       <div class="lorem">
+         <div class="lorem-logo lorem-speed"></div>
+         <h3>Lorem Ipsum</h3>
+         <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+         <a class="learn_more" href="#">Learn more</a>
+       </div>
+       <div class="lorem">
+         <div class="lorem-logo lorem-case"></div>
+         <h3>Lorem Ipsum</h3>
+         <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
+         <a class="learn_more" href="#">Learn more</a>
+       </div>
+     </div>
+    </div>
+
+</body>
+</html>
