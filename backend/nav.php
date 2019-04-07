0e53b518bec847de548cca0555051e3cc873e80a
danielrussocki <46513699+danielrussocki@users.noreply.github.com>
2019-04-07 14:46:18 -0500

    Dashboard
    -usuarios.php funcionando
    -header.php funcionando(Falta cambiar la ruta al insertar)
    -Se eliminaron archivos innecesarios
    -Se modificó el login
    -Imágenes de prueba de inserción en carpeta destino
    -Se modificó _funciones.php
    Se agregó un nav.php que muestra la barra de navegación lateral
    -Se creó estilos.css

---
+++ backend/nav.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Switch</a>
+        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+        <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" id="sign_out">Sign out</a>
+        </li>
+    </ul>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="usuarios.php">
+                            <span data-feather="home"></span>
+                            Usuarios <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="features.php">
+                            <span data-feather="file"></span>
+                            Features
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="works.php">
+                            <span data-feather="file"></span>
+                            Works
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="testimonial.php">
+                            <span data-feather="file"></span>
+                            Testimonial
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="header.php">
+                            <span data-feather="file">
+                            Header
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
